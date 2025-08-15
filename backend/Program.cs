aea170196b459f39ff497645475b4aeefed7d48d
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-08-15 13:34:06 +0300

    add reading hell message from file

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,5 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("hello/", HelloMethod);
 
 app.Run();
+
+string HelloMethod()
+{
+    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
+
+
+
+
+    var file = new FileInfo("hello.txt");
+    var filePath = file.FullName;
+    Console.WriteLine($"Reading hello from: {filePath}");
+
+
+
+
+
+    var message = File.ReadAllText("hello.txt"); 
+
+    return "Read from FILE:\n\n" + message;
+}
