753a200a5668b8c5dcf5c1b89bdd224cecfb3a97
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-08-22 12:10:35 +0300

    add coments to code ETC

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,25 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+// 2
+// We may need to configure builder before building (as an exaxample add controllers etc.)
+// 3
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//4
+// MAP. ENDEPOINT <-> metodi()
+app.MapGet("/", () => "Hello World!"); // using "anomonomous functions"
 app.MapGet("hello/", HelloMethod);
 
+// After run .... program will stop here and wait for GET/POSTS/UPDATE calls ..
+
 app.Run();
+
+// ---------------------------
+Console.WriteLine("this shouls never happen... (is impossible, should be at least)");
+// we will never get here
+
+// what about those
 
 string HelloMethod()
 {
     var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
-
-
+// print to console absolute path (fullname)
 
     var file = new FileInfo("hello.txt");
     var filePath = file.FullName;
     Console.WriteLine($"Reading hello from: {filePath}");
-
-
-
 
 
     var message = File.ReadAllText("hello.txt"); 
